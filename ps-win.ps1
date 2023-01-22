e4fa9867e47c4897ceb4b1f2798ad07941ce86af
animegamer4422 <animegamer4422@gmail.com>
2023-01-23 02:29:50 +0530

    fixed a big typo

--- ps-win.ps1
+++ ps-win.ps1
@@ -7,7 +7,8 @@ if ($PSVersion.Major -lt $latestPSVersion.Major) {
     $isElevated = ([Security.Principal.WindowsIdentity]::GetCurrent()).IsElevated
     if ($isElevated) {
         # code to install the latest version of PowerShell using winget package manager
-        winget install --id=Microsoft.PowerShell -e
+        winget install --id=Microsoft.PowerShell -s=winget
+
         # Set the installed version of PowerShell as the default
         $PSDefault = (Get-ChildItem "HKLM:\SOFTWARE\Microsoft\PowerShell\1\PowerShellEngine\PowerShellVersion" | Where-Object {$_.GetValue("PSVersion") -gt $PSVersion} | Sort-Object -Property PSVersion -Descending | Select-Object -First 1).GetValue("PSVersion")
         Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\powershell.exe" -Name "(Default)" -Value "powershell.exe -Version $PSDefault"
@@ -24,13 +25,10 @@ if ($PSVersion.Major -lt $latestPSVersion.Major) {
 # Check if winget is installed
 if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
     # Set the PowerShell Gallery repository to Trusted
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-powershell /c winget upgrade
-powershell /c winget upgrade --all -h
-$installedPackages = winget show --installed
-$packagesToCheck = @("Microsoft.DesktopAppInstaller_8wekyb3d8bbwe", "Microsoft.VCRedist.2015+.x64", "Microsoft.VCRedist.2015+.x86", "DuongDieuPhap.ImageGlass", "Microsoft.Powershell", "Microsoft.DotNet.DesktopRuntime.6", "Eugeny.Tabby", "Mozilla.Firefox")
-foreach ($package in $packagesToCheck) {
-    if ($installedPackages -notcontains $package) {
-        winget install $package
-    }
+    # Install winget without prompting for confirmation
+    Install-Script -Name winget-install -Force
+    write-host 'Installing Winget'
+    winget-install
 }
