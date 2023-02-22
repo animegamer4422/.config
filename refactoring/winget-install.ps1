9255ed045b88b84a0f8720d51604390527a435f7
animegamer4422 <animegamer4422@gmail.com>
2023-02-22 21:41:39 +0530

    made chatgpt write a if-else loop for winget installation

--- refactoring/winget-install.ps1
+++ refactoring/winget-install.ps1
@@ -1,3 +1,16 @@
-irm -o "winget-install.ps1" "https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1"
+function Install-Winget {
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        Write-Output "winget is already installed."
+    }
+    else {
+        Write-Output "Installing winget..."
+        irm -o "winget-install.ps1" "https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1" ; ./winget-install.ps1
+        if (Get-Command winget -ErrorAction SilentlyContinue) {
+            Write-Output "winget has been installed."
+        }
+        else {
+            Write-Output "winget installation failed."
+        }
+    }
+}
 
-./winget-install.ps1
