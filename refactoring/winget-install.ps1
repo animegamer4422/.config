6eae5dab086b85d9aa76174af7e282dbee342225
animegamer4422 <animegamer4422@gmail.com>
2023-02-22 21:17:00 +0530

    fixed a typo

--- refactoring/winget-install.ps1
+++ refactoring/winget-install.ps1
@@ -3,5 +3,5 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 $env:PSModulePath += ";$([System.IO.Path]::Combine([Environment]::GetFolderPath('MyDocuments'), 'PowerShell\Scripts'))"
 
 Invoke-Command -ScriptBlock {
-    Start-Process powershell.exe -ArgumentList "-Command `"`$env:PSModulePath += ';C:\Program Files\WindowsPowerShell\Scripts'; Install-Script winget-install -Scope AllUsers -Confirm:`$false; winget-install; winget install -h Microsoft.Powershell`""
+    Start-Process powershell.exe -ArgumentList "-Command `"`Install-Script winget-install -Scope AllUsers -Confirm:`$false; winget-install; winget install -h Microsoft.Powershell`""
 } -NoNewScope
