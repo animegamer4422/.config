8948fcd6110590c397058af3ff04e173da84da1b
animegamer4422 <animegamer4422@gmail.com>
2023-02-22 21:04:42 +0530

    removed another useless powershell prompt before installing winget , saying setup scripts installation path

--- refactoring/winget-install.ps1
+++ refactoring/winget-install.ps1
@@ -1,5 +1,7 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Script winget-install
-winget-install
-winget install -h Microsoft.Powershell
+$env:PSModulePath += ";$([System.IO.Path]::Combine([Environment]::GetFolderPath('MyDocuments'), 'PowerShell\Scripts'))"
+
+Invoke-Command -ScriptBlock {
+    Start-Process powershell.exe -ArgumentList "-Command `"`$env:PSModulePath += ';C:\Program Files\WindowsPowerShell\Scripts'; Install-Script winget-install -Scope AllUsers -Confirm:`$false; winget-install; winget install -h Microsoft.Powershell`""
+} -NoNewScope
