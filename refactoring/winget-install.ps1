b20adce28752bf28dac3d623ba790576c7351d35
animegamer4422 <animegamer4422@gmail.com>
2023-02-22 21:32:43 +0530

    literally fixed the winget install by downloading it directly from github and executing it

--- refactoring/winget-install.ps1
+++ refactoring/winget-install.ps1
@@ -1,7 +1,3 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-$env:PSModulePath += ";$([System.IO.Path]::Combine([Environment]::GetFolderPath('MyDocuments'), 'PowerShell\Scripts'))"
+irm -o "winget-install.ps1" "https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1"
 
-Invoke-Command -ScriptBlock {
-    Start-Process powershell.exe -ArgumentList "-Command `"`Install-Script winget-install -Scope AllUsers -Confirm:`$false; winget-install; winget install -h Microsoft.Powershell`""
-} -NoNewScope
+./winget-install.ps1
