90d7c7a6b047b3141bd7d772105017ceaf608b0b
animegamer4422 <animegamer4422@gmail.com>
2023-02-22 21:45:57 +0530

    refactored winget-install and it now works as expected so added it to auto-config.ps1

--- auto-config.ps1
+++ auto-config.ps1
@@ -1,3 +1,21 @@
+function Install-Winget {
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        Write-Output "winget is already installed."
+    }
+    else {
+        Write-Output "Installing winget..."
+        irm -o "winget-install.ps1" "https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1" ; ./winget-install.ps1
+        if (Get-Command winget -ErrorAction SilentlyContinue) {
+            Write-Output "winget has been installed."
+        }
+        else {
+            Write-Output "winget installation failed."
+        }
+    }
+}
+
+Install-Winget
+
 $installedPackages = winget show --installed
 $packagesToCheck = @("Microsoft.Powershell", "Mozilla.Firefox")
 foreach ($package in $packagesToCheck) {
