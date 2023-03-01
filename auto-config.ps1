f37fc72ed26c7531718f43e42ab9f0b00ff70321
Anime Gamer <71986052+animegamer4422@users.noreply.github.com>
2023-03-01 14:19:56 +0530

    wording correction and adding verbose

--- auto-config.ps1
+++ auto-config.ps1
@@ -3,6 +3,7 @@ function Install-Winget {
         Write-Output "winget is already installed."
     }
     else {
+        write-output "Winget not Found"
         Write-Output "Installing winget..."
         irm -o "winget-install.ps1" "https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1" ; ./winget-install.ps1
         if (Get-Command winget -ErrorAction SilentlyContinue) {
